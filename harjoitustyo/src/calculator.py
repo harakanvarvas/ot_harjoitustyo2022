75c591b705c92aebd7c10086f5113abecd9b5d80
harakanvarvas <niksu.hynninen@helsinki.fi>
2022-04-26 17:19:41 +0300

    laskimeen lisätään arvojen tarkistus

--- harjoitustyo/src/calculator.py
+++ harjoitustyo/src/calculator.py
@@ -11,16 +11,20 @@ class Calculator:
 
         #self._result = 0
 
-        if int(last) == 0:
-            self._result = int(amount) * int(frequency)
-        elif int(amount) < 0 or int(frequency) < 0 or int(last) < 0:
+        if last == 0:
+            self._result = amount * frequency
+        elif amount < 0 or frequency < 0 or last < 0:
             return False
-        elif int(amount) == 1:
-            return int(last)
+        elif amount == 1:
+            return last
         else:
-            self._result = (int(amount) - 1) * int(frequency) + int(last)
+            self._result = (amount - 1) * frequency + last
 
         return self._result
+
+    def check_values_amount(self, amount):
+
+
 
     def weeks(days):
         weeks = days // 7
