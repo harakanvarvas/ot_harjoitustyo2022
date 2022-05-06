e0443b0942e9663f6f4b6a5c85da8d8050ceebf9
harakanvarvas <niksu.hynninen@helsinki.fi>
2022-05-07 01:06:23 +0300

    index-virheet korjattu

--- harjoitustyo/src/index.py
+++ harjoitustyo/src/index.py
@@ -3,8 +3,16 @@ from graphic_ui import GraphicUI
 
 def main():
     """Main"""
-    program = GraphicUI()
-    program.start()
+    try:
+        program = GraphicUI()
+    except TypeError:
+        pass
+#        print("TypeError for __init__ requiring 'root'")
+    try:
+        program.start()
+    except UnboundLocalError:
+        pass
+#        print("UnboundLocalError for refering local variable 'program' before assignment")
 
 if __name__ == "__main__":
     main()
