3c1d5cc0164a93cb10cd5bf271a25c4092f29d9d
harakanvarvas <niksu.hynninen@helsinki.fi>
2022-05-11 15:06:25 +0300

    index löytyy

--- harjoitustyo/src/calendar.py
+++ harjoitustyo/src/calendar.py
@@ -17,10 +17,13 @@ def open_eventfile_return_list():
     try:
         with open("events.csv") as datafile:
             for event in datafile:
-                print("open_eventfile event", event)
-                event1 = event.split(";")
-                print("splitevent", event1[0])
-                list1.append(event)
+                event = event.replace("\n", "")
+                event = event.split(";")
+                try:
+                    event_tuple = (event[0], event[1])
+                except IndexError:
+                    pass
+                list1.append(event_tuple)
     except FileNotFoundError:#mikäli tiedostoa ei ole vielä luotu, luodaan se
         with open("events.csv", "w") as datafile:
             pass
@@ -29,33 +32,36 @@ def open_eventfile_return_list():
 
 def write_eventfile(date, event):
     """Lisää funktion argumentteina olevan päivämäärän ja tapahtuman listaan, järjestää listan ja kirjoittaa tapahtumat tiedostoon"""
+    date_event_tuple = (str(date), event)
     date_event = f"{date};{event}\n"#muotoilu
     events = open_eventfile_return_list()#hakee tiedoston listamuodossa
 
-    if date_event not in events:#tarkistaa, että lisättävä tapahtuma ei ole listassa jo
-        date_event = date_event.strip("\n")
-        #print("not in events", date_event)
-        events.append(date_event)
+    if date_event_tuple not in events:#tarkistaa, että lisättävä tapahtuma ei ole listassa jo
+        events.append(date_event_tuple)
     else:
-        print(date_event, "On jo listassa")
+        return False
 
     events.sort()#järjestää listan
 
     with open("events.csv", "w") as datafile:#avataan tiedosto kirjoitettavaksi
         for event in events:
-            date_event = str(event)#tekstimuoto
-            date_event = date_event.strip("\n")
-            date_event = date_event.split(";")
-            datafile.write(f"{date_event[0]};{date_event[1]}\n")
+            datafile.write(f"{event[0]};{event[1]}\n")
 
-    return True #palauttaa arvon True onnistumisen merkiksi
+    return True#palauttaa arvon True onnistumisen merkiksi
+
+def find_event_by_name(name):
+    """Hakee tiedostosta tapahtuman nimellä ja palauttaa sen"""
+    events = open_eventfile_return_list()
+    event = list(filter(lambda x:name in x, events))
+    return event
+
 
 
 def delete_event_by_name(name):
     """Hakee tiedostosta tapahtuman nimellä ja poistaa sen"""
     events = open_eventfile_return_list()
-    index = events.index(name)
-    print(index)
+    index = [events.index(x) for x in events if x[1] == name]
+    return index
 
 def delete_eventfile():
     """Tyhjentää tapahtumatiedoston täydellisesti"""
@@ -73,6 +79,9 @@ write_eventfile(20221205, "Andy (Brachypelma albiceps)")
 write_eventfile(20210131, "Armi")
 write_eventfile(20230101, "Kaaleppi")
 write_eventfile(20220315, "Mango")
-#delete_event_by_name("Armi")
-cont = open_eventfile_return_list()
+inde = delete_event_by_name("Armi")
+print(inde)
+#cont = open_eventfile_return_list()
+#print(cont)
+cont = open_eventfile_return_text()
 print(cont)
